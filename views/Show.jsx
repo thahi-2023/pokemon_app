6a73a7af16f22e86a4154222d392823e04d6d4c1
unknown <thahira.f2021@gmail.com>
2023-08-20 22:53:18 -0400

    Created show views of each pokemon

--- views/Show.jsx
+++ views/Show.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+const myStyle = {
+    color: '#ffffff',
+    backgroundColor: '#000000',
+    };
+
+
 function Show() {
   return (
-    <div>
+    <div style={myStyle}>
         <h1>"Gotta Catch 'Em All"</h1>
+        <h2> {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)} </h2>
+        <img src={ pokemon.img + '.jpg'}/>
+        <a href='/pokemon'> Back </a>
     </div>
   )
 }
