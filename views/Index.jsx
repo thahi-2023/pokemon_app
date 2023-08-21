c2be41a43ec414539913b51b1798ed617ab59aad
unknown <thahira.f2021@gmail.com>
2023-08-20 22:18:58 -0400

    I can view a list of all my pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -11,7 +11,19 @@ function Index() {
   return (
     <div>
         <h1 style={myStyle}>See All The Pokemon!</h1>
+        {
+            pokemon.map((pokemon, i)=> {
+                return (
+                    <ul key={i}>
+                        <li>
+                           <a href={`/pokemon/${i}`}> {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</a> 
+                        </li>
 
+                    </ul>
+                )
+            })
+        }
+           
     </div>
   )
 }
